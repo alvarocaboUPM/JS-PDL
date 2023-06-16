4330d8d9c28d73cccf1f28552018cfc243d5b8d7
alvarocabo <alvarocaboac2@gmail.com>
2023-06-17 01:29:04 +0200

    Fix tests

--- src/test/java/com/pdl/parser/ParseTest.java
+++ src/test/java/com/pdl/parser/ParseTest.java
@@ -1,4 +1,5 @@
 package com.pdl.parser;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -15,39 +16,41 @@ public class ParseTest {
     static Parser p;
     static TS t;
     static String ok, testFile;
-    
 
     @BeforeAll
-    public static void init(){
+    public static void init() {
         t = new SymbolTable();
         p = new Parser(t);
-        
+
     }
 
     @BeforeEach
-    public void resetTestfile(){
+    public void resetTestfile() {
         testFile = Constants.TEST_FOLDER;
         Compiler.errors = new ArrayList<>();
     }
 
     @Test
-    public void testFuncDec(){
-        
+    public void testFuncDec() {
+
     }
 
     @Test
-    public void basicProgramTest(){
+    public void basicProgramTest() {
         testFile += "t1.js";
         ok = "D	 1 10 11 52 13 1 10 11 52 13 1 10 11 54 13 2 14 18 19 38 45 56 2 14 18 19 38 44 48 1 10 11 54 13 2 14 18 19 38 44 48 40 38 44 48 3 26 38 44 48 28 29 14 18 19 38 45 56 2 14 18 19 38 44 48 42 38 44 48 2 15 24 38 44 48 2 15 24 38 44 48 4 ";
+        String res = p.parserDebug(testFile);
         assertTrue(Compiler.errors.isEmpty());
-       
+        assertEquals(ok, res);
     }
 
     @Test
-    public void longIfExpressionTest(){
+    public void longIfExpressionTest() {
         testFile += "t2.js";
-        ok = "D\t 1 10 11 52 13 1 10 11 54 13 1 10 11 52 13 2 15 2 15 24 38 44 48 2 15 2 15 24 38 44 48 42 38 44 48 2 14 18 19 38 44 48 40 38 44 48 3 26 38 44 48 28 29 14 18 19 38 44 48 42 38 45 56 42 38 44 48 42 38 45 56 42 38 46 38 45 56 42 38 44 48 42 38 45 56 4 ";
+        ok = "D\t 1 10 11 52 13 1 10 11 54 13 1 10 11 52 13 2 15 2 15 24 38 44 48 2 15 2 15 24 38 44 48 42 38 44 48 2 14 18 19 38 44 48 40 38 44 48 3 26 38 44 48 28 29 14 18 19 38 44 48 42 38 45 56 42 38 44 48 42 38 45 56 42 38 46 38 45 56 42 38 44 48 42 38 45 56 40 38 45 56 4 ";
+        String res = p.parserDebug(testFile);
         assertTrue(Compiler.errors.isEmpty());
+        assertEquals(ok, res);
     }
 
 }
