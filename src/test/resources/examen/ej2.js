72e59fd001ca55cce811e780cf3e3d369eb856d1
Álvaro Cabo <criptocrazy8@gmail.com>
2023-06-19 18:25:29 +0200

    Added test files

---
+++ src/test/resources/examen/ej2.js
@@ -0,0 +1,47 @@
+/** No se debe cambiar el ejemplo hasta que lo indique el profesor **/
+let string s ;let a int;
+let bb boolean;
+a= 9 > a + 3;
+
+function RR int (number n, boolean b)
+{
+	let log boolean;
+	log= n < b ;
+	input (kk);
+	if (b && log)	return log;
+	return n - RR (n - 1, bb);	
+}
+
+let s_s string ;
+
+RR = 1;
+
+function print (string s, int RR)	
+{
+	print(s);
+	let s_s boolean;
+	print"hola;
+	print a -3;
+	print RR;	input s;
+	if (bb)return kk;
+	print (0132765 + (8 +3)+a);
+}
+
+s_s = "++++++Helloooooooooooooooooooooooooooooooooooooooooooooooooooooooooo";
+
+function  cadena string (string kk)
+{
+	while (a > 9 || bb) 
+	{prin ('{9}', RR(s, bb));
+		let	xx	int;
+		xx=s_s;
+		if (bb) if(bb) return kk;
+	}
+	else      
+		{return "";} 
+
+
+	s$= RR (a, bb, kk);
+	kk = prin (kk,99));
+	return s;
+
---
+++ src/test/resources/examen/ej4.js
@@ -0,0 +1,57 @@
+//** No se debe cambiar el ejemplo hasta que lo indique el profesor **/
+let  s string;let a int ;
+let bb boolean;
+
+
+function  int RR (int n, boolean _b)
+{
+	input(kk);
+	let log boolean;
+	log= n < a < 32100	;	
+	if (_b && log)	return s;
+	return n + RR (n + 1, bb);	
+}
+
+let s_s string;
+
+function print (string s, int RR)	
+{
+	let s_s boolean;
+	
+	print"++++Helloooooooooooooooooooooooooooooooooooooooooooooooooooooooooo";
+	RR= (8.8);
+		print(s)
+	;if (bb) return kk;
+	print RR;		print (4 + (a + 0987)) +3);
+}
+
+kk=332211;
+
+RR = 1;
+
+function  cadena string(string kk)
+{
+	if ((a != 091) || bb) 
+	{kk= prin ('{$}', RR(a, bb));
+	let xx string;
+	xx= kk;
+	if (bb) if (bb) 
+		return xx;
+	}
+	      
+		return "";
+	xx= s_s;
+
+
+
+
+s = ";
+a= RR (5, bb, kk);
+prin (kk, 99);
+return s;
+let a boolean ;
+
+a= 91 < 8 + a;
+
+
+
